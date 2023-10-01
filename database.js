3feed1f0b8d8e91d3d662d36548105862d10e382
hetyey-b <hetyey.botond@gmail.com>
2023-10-01 13:17:41 +0200

    endpoints

--- database.js
+++ database.js
@@ -21,13 +21,22 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
                 var insert = 'INSERT INTO user (name, score) VALUES (?,?,?)'
                 db.run(insert, ["test1",1])
                 db.run(insert, ["test2",2])
-                db.run(insert, ["test3",3])
+                db.run(insert, ["test3",32])
                 db.run(insert, ["test4",4])
                 db.run(insert, ["test5",5])
                 db.run(insert, ["test6",6])
-                db.run(insert, ["test7",7])
-                db.run(insert, ["test8",8])
-                db.run(insert, ["test9",9])
+                db.run(insert, ["test7",12])
+                db.run(insert, ["test8",10])
+                db.run(insert, ["test9",12])
+                db.run(insert, ["test10",9])
+                db.run(insert, ["test11",17])
+                db.run(insert, ["test12",13])
+                db.run(insert, ["test13",31])
+                db.run(insert, ["test14",12])
+                db.run(insert, ["test15",21])
+                db.run(insert, ["test16",11])
+                db.run(insert, ["test17",20])
+                db.run(insert, ["test18",11])
             }
         });  
     }
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require("express")
 var app = express()
 var db = require("./database.js")
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Server port
 var HTTP_PORT = 8000 
 // Start server
@@ -13,6 +17,51 @@ app.listen(HTTP_PORT, () => {
 app.get("/", (req, res, next) => {
     res.json({"message":"Ok"})
 });
+
+app.get("/leaderboard", (req,res,next) => {
+    var sql = "select * from leaderboard limit 10 order by score desc"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+    });
+});
+
+app.post("/leaderboard", (req, res, next) => {
+    var errors=[]
+    if (!req.body.name){
+        errors.push("No name specified");
+    }
+    if (!req.body.score){
+        errors.push("No score specified");
+    }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+    var data = {
+        name: req.body.name.replaceAll(/['><()&!@#$%]/g,""),
+        score: parseInt(req.body.score),
+    }
+    var sql ='INSERT INTO leaderboard (name, score) VALUES (?,?)'
+    var params =[data.name, data.score]
+    db.run(sql, params, function (err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+        })
+    });
+})
+
 
 // Insert here other API endpoints
 
